29de43dcc5158e422ecbf407b26a1cf060057ad3
perimetro <alegene2004@gmail.com>
2022-04-01 12:32:41 +0200

    Update Mano.java

--- src/Gioco_di_Carte/Mano.java
+++ src/Gioco_di_Carte/Mano.java
@@ -3,11 +3,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Gioco_di_Carte;
-
+import java.util.*;
 /**
  *
  * @author 
  */
 public class Mano {
+    private int misura;
+    public ArrayList<Carta>gestione;
+    public Mano(){
+        gestione=new <Carta>ArrayList();      
+    }
+    public int size(){
+        return gestione.size();
+    }
+    
     
 }
