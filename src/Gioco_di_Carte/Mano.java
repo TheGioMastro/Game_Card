686c5d3de9af82251f4e39fae142d1761f744837
Giuliano <gold3ngt@outlook.it>
2022-04-01 11:08:46 +0200

    Aggiunta Classi

---
+++ src/Gioco_di_Carte/Mano.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Gioco_di_Carte;
+
+/**
+ *
+ * @author Giuliano Tommaso Colombo <colombogiulianotommaso@itis-molinari.eu>
+ */
+public class Mano {
+    
+}
