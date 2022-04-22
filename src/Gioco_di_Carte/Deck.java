cb382f6fb56625bf669266e8890065d2f2d4d159
Giuliano <gold3ngt@outlook.it>
2022-04-22 12:23:54 +0200

    Modifica completa alla classe

--- src/Gioco_di_Carte/Deck.java
+++ src/Gioco_di_Carte/Deck.java
@@ -8,17 +8,19 @@ package Gioco_di_Carte;
  * @author Generali Alessandro
  */
 public class Deck {
-    protected Descrizione[] Insieme_descrizioni = new Descrizione[15];
-    public Deck(){
-        for(int i=0; i<15; i++){
-            Insieme_descrizioni[i]= new Descrizione();
-        }           
-    }
-
-    public Descrizione getInsieme_descrizioni(int i) {
-        return Insieme_descrizioni[i];
-    }
-
+    protected Carta[] dck;
+    protected final int nCarte = 15;
     
+    public Deck(Mazzo m1){
+        int i;
+        dck = new Carta[nCarte];
+        
+        for(i = 0; i < nCarte; i++){
+            dck[0] = m1.Get(i);
+        }
+    }
     
+    public Carta getCarta(int i){
+        return dck[i];
+    }
 }
