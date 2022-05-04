263a004b79d880ad489eb641960c0988f8a4a40a
Alessandro <alessandro.poggi003@gmail.com>
2022-05-05 00:04:59 +0200

    sistemato bug sull'attacco della classe gioco
    - rimesso valore standard di p1attack e p2attack perché altrimenti non si vedeva il tasto destro
    - effettuato il controllo se la carta è una magia o meno perché altrimenti si rischiava di cadere in oggetti non istanziati

--- src/Gioco_di_Carte/Gioco.java
+++ src/Gioco_di_Carte/Gioco.java
@@ -19,7 +19,6 @@ public class Gioco {
     protected static int nTurno; 
     protected int p1attack;
     protected int p2attack;
-    private boolean semaphore;
     
     
     //Grafica del gioco
@@ -170,14 +169,12 @@ public class Gioco {
             grafica.hide_GUI(giocatore_1, false, giocatore_2, true);
             
         }else if(nTurno % 2 == 1){//Turno Giocatore 1
-            semaphore = true;
             this.p1attack = 1;
             for(i=0; i < giocatore_1.getCim().Size(); i++){
-                if(giocatore_1.getCim().Get(i).getMagia().check2P() && semaphore){
-                    this.p1attack = 2;
-                    semaphore = false;
-                }else{
-                    semaphore = false;
+                if(giocatore_1.getCim().Get(i).getTipo_Carta().equals("Magia")){
+                    if(giocatore_1.getCim().Get(i).getMagia().check2P()){
+                        this.p1attack = 2;
+                    }
                 }
             }
             
@@ -191,14 +188,12 @@ public class Gioco {
             grafica.hide_GUI(giocatore_1, false, giocatore_2, true);
             
         }else{//Turno Giocatore 2
-            semaphore = true;
-            this.p1attack = 1;
+            this.p2attack = 1;
             for(i=0; i < giocatore_2.getCim().Size(); i++){
-                if(giocatore_2.getCim().Get(i).getMagia().check2P() && semaphore){
-                    this.p1attack = 2;
-                    semaphore = false;
-                }else{
-                    semaphore = false;
+                if(giocatore_2.getCim().Get(i).getTipo_Carta().equals("Magia")){
+                    if(giocatore_2.getCim().Get(i).getMagia().check2P()){
+                        this.p2attack = 2;
+                    }
                 }
             }
             
